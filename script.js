3b322a983224551d6fd51a42d7985d81ef7b5808
ndrada <andrada.groseanu@gmail.com>
2023-09-20 06:19:35 -0700

    finished app

--- script.js
+++ script.js
@@ -5,5 +5,40 @@ let flag = true;
 let destination = document.getElementById('typedtext');
 
 function loadQuote(){
-    const url =''
+    const url = 'https://api.quotable.io/random';
+
+    fetch(url)
+
+    .then(response => {
+        if(response.ok) {
+            return response.json();
+        } else {
+            console.log(response.status);
+        }
+    })
+
+    .then(data => {
+        quoteArray[index] = data.content;
+    })
 }
+
+function typewriter(){
+    if (flag){
+        loadQuote();
+        quoteArray[index] += ' ';
+        flag = false;
+    }
+
+    destination.innerHTML = quoteArray[index].substring(0, textPosition) + '<span>\u25AE</span>';
+
+    if(textPosition++ != quoteArray[index].length){
+        setTimeout('typewriter()', 100);
+    } else {
+        quoteArray[index] = ' ';
+        setTimeout('typewriter()', 3000);
+        textPosition = 0;
+        flag = true;
+    }
+}
+
+typewriter();
